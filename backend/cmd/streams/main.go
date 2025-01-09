68657ff7ec2670b43fd7f848ba9ede8926daadfe
marcoaureliojf <marcoaurelio.fjf@gmail.com>
2025-01-09 11:52:34 -0300

    Refactor environment variable handling and clean up test files

--- backend/cmd/streams/main.go
+++ backend/cmd/streams/main.go
@@ -5,12 +5,18 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/marcoaureliojf/streamStudio/backend/internal/config"
 	"github.com/marcoaureliojf/streamStudio/backend/internal/database"
 	"github.com/marcoaureliojf/streamStudio/backend/internal/routes"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
+
 	cfg := config.LoadConfig()
 	database.Connect(cfg)
 
